1d62412b4943282a90fd1cfff05170c6283ea2b2
edsonni1095 <edsonni1095@gmail.com>
2024-11-28 01:57:33 +0000

    actualizar pagina

--- main.py
+++ main.py
@@ -35,3 +35,21 @@ st.latex("a^2 + b^2= c^2")
 
 st.image("https://th.bing.com/th/id/OIP.kPLj7HLR9LX2O2rGClWmjgHaIT?rs=1&pid=ImgDetMain")
 st.video("https://www.youtube.com/watch?v=eTEBvBIz8Ok")
+
+# layout
+#container
+
+st.markdown("formula del estudiante:")
+st.latex(r"x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}")
+
+#columnas
+
+c1, c2 = st.columns(2)
+
+with c1: 
+     st.markdown("texto en la primera columna")
+     st.title("titulo")
+     st.latex("f(x)= x^x")
+with c2:
+     st.markdown("texto en la segunda columna")
+     st.image("https://th.bing.com/th/id/OIP.fjyhi67Ivh6fF0B6QnSHAQAAAA?rs=1&pid=ImgDetMain")
