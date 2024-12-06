20526f994f811bf9e8b27551edca8878fddcc88a
edsonni1095 <edsonni1095@gmail.com>
2024-12-06 03:47:27 +0000

    Actualizar Pagina

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import streamlit as st
 intro = st.Page("paginas/intro.py", title="Introduccion", icon=":material/description:")
 ejemplos = st.Page("paginas/ejemplos.py", title="Ejemplos", icon=":material/function:")
 inputs = st.Page("paginas/inputs.py", title="Inputs", icon=":material/calculate:")
+cuest = st.Page("paginas/cuest.py", title="Cuestionario", icon=":material/question_answer:")
 
-pg = st.navigation([intro, ejemplos, inputs])
+pg = st.navigation([intro, ejemplos, inputs, cuest])
 
 pg.run()
---
+++ paginas/cuest.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as patches
+import pandas as pd 
+import sympy as sy
+
+
+
